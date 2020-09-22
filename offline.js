021558a558fd5590bb7acd6cf1174173fa25b27b
semicon <1210@sts.ac.th>
2020-09-22 08:56:56 +0700

    resistor
    resistor

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1600739338,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bar1-sheet0.png",
+		"images/bar1-sheet1.png",
+		"images/bar1-sheet2.png",
+		"images/resistor-sheet0.png",
+		"images/btn-sheet0.png",
+		"images/btn-sheet1.png",
+		"images/symbolcheck-sheet0.png",
+		"images/symbolcheck-sheet1.png",
+		"images/symbolcheck-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
